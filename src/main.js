7434a7e85d4a37ce542015be407cc1193aaffbd7
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-06-06 20:44:38 -0300

    create state for time and date

--- src/main.js
+++ src/main.js
@@ -10,6 +10,16 @@ const store = createStore({
       price: '',
       date: 1641006000,
       priceOnDate: '',
+      dateObj: {
+        day: 0,
+        month: 0,
+        year: 0
+      },
+      timeObj: {
+        hour: 0,
+        minute: 0,
+        seconds: 0
+      },
     }
   },
   mutations: {
@@ -25,6 +35,12 @@ const store = createStore({
     setPriceOnDate(state, newPriceOnDate) {
       state.priceOnDate = newPriceOnDate;
     },
+    setDateObj(state, newDateObj) {
+      state.dateObj = newDateObj;
+    },
+    setTimeObj(state, newTimeObj) {
+      state.timeObj = newTimeObj;
+    },
   },
   actions: {
     setPrice({ commit }, newPrice) {
@@ -35,7 +51,13 @@ const store = createStore({
     },
     setPriceOnDate({ commit }, newPriceOnDate) {
       commit('setPriceOnDate', newPriceOnDate)
-    }
+    },
+    setDateObj({ commit }, newDateObj) {
+      commit('setDateObj', newDateObj);
+    },
+    setTimeObj({ commit }, newTimeObj) {
+      commit('setTimeObj', newTimeObj);
+    },
   }
 })
 

0d0274db5d7f7df2189b89f53adda4bf1d98be02
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-06-06 20:44:01 -0300

    fix background color

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,21 @@
 </script>
 
 <template>
-  <Background />
-  <Header />
-  <main>
-    <CryptoContent />
-  </main>
+  <div class="opacityBG">
+    <Background />
+    <Header />
+    <main>
+      <CryptoContent />
+    </main>
+  </div>
 </template>
 
 <style>
 
+.opacityBG {
+  position: absolute;
+  background-color: rgba(32,32,32,0.8);
+  width: 100%;
+  height: 100vh;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,18 +4,24 @@
 <template>
   <header class="">
   <div class="relative">
-    <img class="object-cover -scale-x-100 h-36" src="../assets/waveBg.svg">
+    <img
+      src="../assets/waveBg.svg"
+    >
     <h1 class="
       w-screen
       text-center
       absolute
       top-0
       left-0
-      py-5
+      py-3
       font-sans
       font-bold
       text-3xl
       text-neutral-500
+      sm:text-left
+      sm:px-20
+      sm:py-20
+      sm:text-8xl
     ">
       CryptoCurrency
     </h1>

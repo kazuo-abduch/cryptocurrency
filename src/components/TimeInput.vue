c1ffb792fe2cdb7309818e744c3659078c7e490e
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-06-06 20:44:30 -0300

    Refactor = create component TimeInput

---
+++ src/components/TimeInput.vue
@@ -0,0 +1,50 @@
+<script>
+  import { mapState } from 'vuex';
+  export default {
+    computed: {
+      ...mapState({
+        timeObj: state => state.timeObj,
+      })
+    },
+    methods: {
+      listenTime({ target }) {
+        if (target.placeholder === 'hour') {
+          if (target.value > 23) {
+            target.value = 23;
+            if (target.value < 0) {
+              target.value = 0;
+            }
+          }
+          this.$store.commit('setTimeObj', { ...this.$store.state.timeObj, hour: target.value });
+        };
+        if (target.placeholder === 'minute') {
+          if (target.value > 59) {
+            target.value = 59;
+            if (target.value < 0) {
+              target.value = 0;
+            }
+          }
+          this.$store.commit('setTimeObj', { ...this.$store.state.timeObj, minute: target.value });
+        };
+        if (target.placeholder === 'seconds') {
+          if (target.value > 59) {
+            target.value = 59;
+            if (target.value < 0) {
+              target.value = 0;
+            }
+          }
+          this.$store.commit('setTimeObj', { ...this.$store.state.timeObj, seconds: target.value });
+        };
+      },
+    },
+  }
+</script>
+
+<template>
+  <div class="">
+    <input placeholder='hour' type="number" max="23" min="0" @change="listenTime" />
+    <input placeholder='minute' type="number" max="59" min="0" @change="listenTime" />
+    <input placeholder='seconds' type="number" max="59" min="0" @change="listenTime" />
+    <button type="button" @click="getPriceByDate" >Confirm</button>    
+  </div>
+</template>

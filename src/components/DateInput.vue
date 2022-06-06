3b6d2c35ff840347e36fe545f8994b0db426d52b
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-06-06 20:44:17 -0300

    Refactor = create component DateInput

---
+++ src/components/DateInput.vue
@@ -0,0 +1,28 @@
+<script>
+  import { mapState } from 'vuex';
+  export default {
+    computed: {
+      ...mapState({
+        dateObj: state => state.dateObj,
+      })
+    },
+    methods: {
+      listChangeDate({ target }) {
+        console.log(Date.now());
+        const dateArray = target.value.split('-');
+        const newDateObj = {
+          day: Number(dateArray[2]),
+          month: Number(dateArray[1]),
+          year: Number(dateArray[0]),
+        }
+        this.$store.commit('setDateObj', newDateObj);
+      },
+    },
+  }
+</script>
+
+<template>
+  <div class="">
+    <input type=date @change="listChangeDate"/>
+  </div>
+</template>

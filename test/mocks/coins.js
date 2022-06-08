6673f658072015aea262fce138edff71e3bc19c6
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-06-08 18:33:00 -0300

    add Jest and coin mock

---
+++ test/mocks/coins.js
@@ -0,0 +1,9 @@
+const coins = {
+  "bitcoin":{"usd":30328},
+  "dacxi":{"usd":0.00216817},
+  "ethereum":{"usd":1802.38},
+  "cosmos":{"usd":8.85},
+  "terra-luna-2":{"usd":2.81},
+}
+
+module.export = coins;

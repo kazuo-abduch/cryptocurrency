99dd00f44c97cb804ff69d2ca966f09a02b7d96a
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-06-08 19:18:33 -0300

    remove jest

--- test/mocks/coins.js
+++
@@ -1,9 +0,0 @@
-const coins = {
-  "bitcoin":{"usd":30328},
-  "dacxi":{"usd":0.00216817},
-  "ethereum":{"usd":1802.38},
-  "cosmos":{"usd":8.85},
-  "terra-luna-2":{"usd":2.81},
-}
-
-module.export = coins;
